0ab40122017a71a5196da8dc1d373db58f98fdd4
Mykola Gurov <ngurov@gmail.com>
2016-08-15 23:05:26 +0200

    refactoring

--- routes/webhook.handler.js
+++ routes/webhook.handler.js
@@ -3,37 +3,35 @@ var cmdParser = require('../lib/cmdParser.js');
 var Repository = require('../lib/repository.js');
 
 
-function done(res) {
-  return function () { res.sendStatus(200); };
-}
-
 module.exports = function (hipchat) {
 
-  return function handler(req, res) {
-    var cmd = cmdParser.parseTimerCommand(req.body.item.message.message);
-
+  function processCmd(cmd) {
     console.log(cmd);
 
     if (cmd.error) {
-      hipchat.sendMessage(req.clientInfo, req.identity.roomId, cmd.error, { format: 'text', color: 'red', notify: false })
-        .then(done(res));
+      return hipchat.sendMessage(req.clientInfo, req.identity.roomId, cmd.error, { format: 'text', color: 'red', notify: false });
     } else {
       var timerText = cmd.text;
 
-      Repository.persistTimer(
+      return Repository.persistTimer(
         {
           args: [req.clientInfo, req.identity.roomId, timerText, { format: 'text', color: 'green', notify: true }],
           timestamp: cmd.executionTime.getTime()
         }
       ).then(function () { console.log('returning ok'); return null; }, function (err) { console.log('returning err'); return err; })
-      .then(
+        .then(
         function (err) {
           //TODO err callback and response
           var text = !!err ? "Error" : "OK " + moment(cmd.executionTime).tz('Europe/Amsterdam').format('HH:mm');
           var color = !!err ? "red" : "green";
           return hipchat.sendMessage(req.clientInfo, req.identity.roomId, text, { format: 'text', color: color, notify: false });
-        })
-        .then(done(res));
+        });
     }
+  }
+
+  return function handler(req, res) {
+    var cmd = cmdParser.parseTimerCommand(req.body.item.message.message);
+    processCmd(cmd)
+      .then(function () { res.sendStatus(200); });
   }
 } 
